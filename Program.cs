88c2c88fd10887a11b0417d2f62a8f28c972be0a
Matthew Sleith <21234957@shrewsbury.ac.uk>
2022-06-20 09:52:01 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Visual_studio_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wantednum = 4;
+            int[] intarray = {'1','2','3','4','5','6','7','8','9','10'};
+            bool found = false;
+
+        }
+    }
+}
